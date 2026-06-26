b9986b7d364befeaf57e6471ca31f83bb8d178f8
AmanByte1 <amandiwan775@gmail.com>
2026-06-26 23:40:19 +0530

    hashing

---
+++ Revision/Hashing/eg.py
@@ -0,0 +1,21 @@
+s={
+    "name":"aman",
+    "age":20,
+    "branch":"CSE"
+}
+print(s["name"])
+
+
+mp = {
+    10: True,
+    20: True,
+    30: True,
+    40: True
+}
+print(30 in mp)
+
+
+mp["apple"] = 10
+
+if "apple" in mp:
+    print("Found")
